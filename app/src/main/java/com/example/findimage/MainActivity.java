f0cdd2a0fffddcc5b64b8c245f37f0c3ad801515
ArtursKrasts <AKSturmovik@gmail.com>
2020-04-20 17:21:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/findimage/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.findimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    private static final int PICK_IMAGE = 100;
+    Uri imageURI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView1);
+    }
+
+    public void FindImage(View view) {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageURI = data.getData();
+            imageView.setImageURI(imageURI);
+        }
+    }
+}
